3fb0f0f9f5e40257990d3718318046cf453752d8
iidiong <idong30@hotmail.com>
2021-12-24 19:55:04 -0500

    fixed issue with unit test and added new test

--- flask-blogly/test_app.py
+++ flask-blogly/test_app.py
@@ -1,7 +1,7 @@
-from unittest import TestCase
+import unittest
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = True
@@ -13,7 +13,7 @@ db.drop_all()
 db.create_all()
 
 
-class UserModelTestCase(TestCase):
+class UserModelTestCase(unittest.TestCase):
     def setUp(self):
         User.query.delete()
 
@@ -27,6 +27,18 @@ class UserModelTestCase(TestCase):
     def tearDown(self):
         db.session.rollback()
 
+    def test_list_users_data(self):
+        with app.test_client() as client:
+            resp = client.get("/users")
+            self.assertEqual(resp.status_code, 200)
+            self.assertTrue("Henry" in resp.get_data(as_text=True))
+            
+    def test_add_new_users_form(self):
+        with app.test_client() as client:
+            resp = client.get("/users/new")
+            self.assertEqual(resp.status_code, 200)
+            self.assertTrue("Create a user" in resp.get_data(as_text=True))
+
     def test_list_users_redirect(self):
         with app.test_client() as client:
             resp = client.get("/")
@@ -37,12 +49,21 @@ class UserModelTestCase(TestCase):
             resp = client.get("/users")
             self.assertEqual(resp.status_code, 200)
 
-    def test_show_user(self):
+    def test_new_user_with_no_post(self):
         with app.test_client() as client:
             resp = client.get(f"/users/{self.user_id}")
-            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.status_code, 302)
 
     def test_show_invalid_user(self):
         with app.test_client() as client:
             resp = client.get("/users/100")
-            self.assertEqual(resp.status_code, 404)
+            self.assertEqual(resp.status_code, 302)
+    
+    def test_delete_user(self):
+        with app.test_client() as client:
+            resp = client.get(f"/users/{self.user_id}/delete")
+            self.assertEqual(resp.status_code, 302)
+            self.assertTrue("Users", resp.get_data(as_text=True))
+
+
+
